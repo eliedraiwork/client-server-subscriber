d0a5416a428d4247f62293f33290b5ba2cde8ce0
Elie Drai <elie.drai.work@gmail.com>
2020-08-30 16:17:51 +0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+//  SERVER (implemented in node.js)
+
+//  The server will have 2 main routes:
+//  -> POST create customer (should return status 200 OK if everything fine and error 404 otherwise)
+//  -> GET all country values
+
